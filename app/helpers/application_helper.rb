9dd8689b68492c0f43bcbf8df54a22a0c0e5ed17
jonathanmpope <jonathanmpope@gmail.com>
2023-04-05 12:27:50 -0600

    feat: add video links for email - toe touch assessment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,25 @@ module ApplicationHelper
     def toe_touch_movements
         if @user.toe_touch_question_one == 'no'
             @toe_touch_one = "https://www.youtube.com/embed/-a5d7FPkuqI"
+            @toe_touch_one_short = "https://youtu.be/HVYHlKohTAM"
+            @toe_touch_one_long = "https://youtu.be/-a5d7FPkuqI"
             @toe_touch_two = "https://www.youtube.com/embed/3MZVfb82Vzk"
+            @toe_touch_two_short = "https://youtu.be/KnfWzOiq20g"
+            @toe_touch_two_long = "https://youtu.be/3MZVfb82Vzk"
         elsif @user.toe_touch_question_one == 'yes' && @user.toe_touch_question_two == 'no'
             @toe_touch_one = "https://www.youtube.com/embed/7GkCqNRGcNs"
+            @toe_touch_one_short = "https://youtu.be/X41a8V0MjVs"
+            @toe_touch_one_long = "https://youtu.be/7GkCqNRGcNs"
             @toe_touch_two = "https://www.youtube.com/embed/PcMzCqP7Wh4"
+            @toe_touch_two_short = "https://youtu.be/CD5A15-N2fo"
+            @toe_touch_two_long = "https://youtu.be/PcMzCqP7Wh4"
         elsif @user.toe_touch_question_one == 'yes' && @user.toe_touch_question_two == 'yes' && @user.toe_touch_question_three == 'no'
             @toe_touch_one = "https://www.youtube.com/embed/-WUK091r37Y"
+            @toe_touch_one_short = "https://youtu.be/MUe48st6tgM"
+            @toe_touch_one_long = "https://youtu.be/-WUK091r37Y"
             @toe_touch_two = "https://www.youtube.com/embed/9VDPKmqtApM"
+            @toe_touch_two_short = "https://youtu.be/20J4lRe3Igw"
+            @toe_touch_two_long = "https://youtu.be/9VDPKmqtApM"
         else
             @toe_touch_one = nil 
             @toe_touch_two = nil 
